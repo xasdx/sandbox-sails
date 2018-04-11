b0ebbb3568eb450bcce08b2b97446ae894e73d91
_asdx_ <xasdx@tuta.io>
2018-04-11 11:54:44 +0000

    Use chakram for api tests

--- test/integration/alias.test.js
+++ test/integration/alias.test.js
@@ -1,8 +1,13 @@
-let { expect } = require("chai")
+let { get, expect, wait } = require("chakram")
 let sails = require("sails")
 
 module.exports = {
   "before": done => sails.lift({}, done),
-  "specifies functionality": () => expect(true).to.be.true,
+  "returns aliases": () => {
+    let res = get("http://localhost:8080/aliases")
+    expect(res).to.have.status(200)
+    expect(res).to.have.schema({ "type": "array" })
+    return wait()
+  },
   "after": done => sails.lower(done)
 }
